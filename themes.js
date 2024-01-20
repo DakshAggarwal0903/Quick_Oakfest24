f48057fe39b013e1810ce5be6d5ed549f7403a7a
ionlymadethisaccountforjavascript <108260879+ionlymadethisaccountforjavascript@users.noreply.github.com>
2024-01-20 17:33:55 +0530

    themes.js

---
+++ themes.js
@@ -0,0 +1,8 @@
+function darktheme1(){
+    var element = document.body;
+    element.classList.toggle('darkmode1')
+}
+function darktheme2(){
+    var element = document.body;
+    element.classList.toggle('darkmode2')
+}
