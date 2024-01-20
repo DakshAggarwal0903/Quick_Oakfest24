1e3fa8d3d8b1486a70540f7e5d297301ff616f93
quick no. 2 <157027960+ilyxr@users.noreply.github.com>
2024-01-20 17:47:36 +0530

    temp remove linear. headline repetition patched

--- combination_news.py
+++ combination_news.py
@@ -1,4 +1,3 @@
-import openpyxl
 import newsapi
 import yfinance as yf
 import pandas as pd
@@ -8,70 +7,77 @@ from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Set up NLTK
+# Set up NLTK for NLP. Sentiment used for values later on
+# Set up NLTK for NLP
 nltk_stopwords = set(stopwords.words('english'))
 sid = SentimentIntensityAnalyzer()
 
-# Set up newsapi client
-api_key = '4108b59008174aa6bae856911471dda4'
+# Set up newsapi
+#i hate commenting
+api_key = 'fcecf50fcd844427b98a50db33f5ed42'
 newsapi.NewsApiClient(api_key)
 
 # Set up yfinance to download stock data
+#yfinance up
 ticker = str(input("give ticker to trace - "))
 stock_data = yf.download(ticker, start='2023-12-20', end=datetime.now(), interval='1h')
 
-# Get the current price of the stock
+#price setup
+#stock price
 last_price = stock_data['Close'].iloc[-1]
 
-# Set up data structures to store stock and headline data
+#i sleep here is the headline data array
+#data
 headline_data = []
 
-# Get news headlines for the past year
+# Get news headlines also newsapi cringe as hell man why only december 20
+#give me ur headlines
 news_api = newsapi.NewsApiClient(api_key)
 query = ticker
-articles = news_api.get_everything(q=query, language='en', sort_by='relevancy', from_param='2023-12-20')
+articles = news_api.get_everything(q=query, language='en', sort_by='relevancy', from_param='2024-01-10')
 headlines = [article['title'] for article in articles['articles']]
-
-# Iterate through the headlines and find the hourly change in the stock price after each headline
+unique_headlines = set()
 for headline in headlines:
     for i in range(len(stock_data)-1, -1, -1):
-        if (stock_data.index[i].replace(tzinfo=None) > ((datetime.now() - timedelta(hours=24))).replace(tzinfo=None)):
-            # If the headline was published in the last 24 hours, find the hourly change
+        if (stock_data.index[i].replace(tzinfo=None) > ((datetime.now() - timedelta(hours=48))).replace(tzinfo=None)):
             last_headline_price = stock_data.iloc[i]['Close']
             hourly_change = (stock_data.iloc[i]['Close'] - last_price) / last_price
-            headline_data.append({'Headline': headline, 'Hourly Change': hourly_change})
-            last_price = stock_data.iloc[i]['Close']
+            if headline not in unique_headlines:
+                headline_data.append({'Headline': headline, 'Hourly Change': hourly_change})
+                last_price = stock_data.iloc[i]['Close']
+                #nlp scoring code uwuww aughagahhaga
+                words = word_tokenize(headline.lower())
+                filtered_words = [word for word in words if word.isalnum() and word not in nltk_stopwords]
+                freq_dist = FreqDist(filtered_words)
+                important_words = [word for word in filtered_words if freq_dist[word] > 1]
+                importance_score = len(important_words) / len(filtered_words)
+                sentiment_score = sid.polarity_scores(headline)['compound']
 
-            words = word_tokenize(headline.lower())
-            filtered_words = [word for word in words if word.isalnum() and word not in nltk_stopwords]
-            freq_dist = FreqDist(filtered_words)
-            important_words = [word for word in filtered_words if freq_dist[word] > 1]
-            importance_score = len(important_words) / len(filtered_words)
-            sentiment_score = sid.polarity_scores(headline)['compound']
+                #scores for headlines
+                headline_data[-1]['Importance Score'] = importance_score #doesnt work ever
+                headline_data[-1]['Sentiment Score'] = sentiment_score
+                unique_headlines.add(headline)
+            else:
+                print("hi")
 
-            # Add the scores to the headline data
-            headline_data[-1]['Importance Score'] = importance_score
-            headline_data[-1]['Sentiment Score'] = sentiment_score
-
-# Create dataset
+#dataframesetthingy
 data = [{'Date': stock_data.index[i], 'Open': stock_data.iloc[i]['Open'], 'High': stock_data.iloc[i]['High'],
-         'Low': stock_data.iloc[i]['Low'], 'Close': stock_data.iloc[i]['Close'], 'Adj Close': stock_data.iloc[i]['Adj Close'], 'Diff': (stock_data.iloc[i]['Close']-stock_data.iloc[i]['Open']),
+         'Low': stock_data.iloc[i]['Low'], 'Close': stock_data.iloc[i]['Close'], 'Adj Close': stock_data.iloc[i]['Adj Close'],
          'Volume': stock_data.iloc[i]['Volume']} for i in range(len(stock_data))]
 df = pd.DataFrame(data)
 
-# Add the headline data to the dataframe
+
+
+#become one
 headline_df = pd.DataFrame(headline_data)
 df = pd.concat([df, headline_df], axis=1)
 
 print(df.head(20))
 
-# Set the filename
+#foolname
 filename = ('output_'+ticker+'_stock.json')
-
-# Use the to_json function to export the dataframe
-df.to_json(filename, orient='records')
-
 ###
+###i hate this
 ###
 ###ai
 ### getting latest news
@@ -95,26 +101,6 @@ headline_v2 = get_latest_news(ticker_input=ticker)
 
 words_v2 = word_tokenize(headline_v2.lower())
 sentiment_score_v2 = sid.polarity_scores(headline_v2)['compound']
-
-###print(headline_v2 + str(sentiment_score_v2))
-###dead code
-####
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-
-# Split the data into training and testing sets
-X = df['Sentiment Values'].values.reshape(-1, 1)
-y = df['Close'].values.reshape(-1, 1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train the linear regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = model.predict(X_test)
 
 # Calculate the R-squared value
 r2 = r2_score(y_test, y_pred)
