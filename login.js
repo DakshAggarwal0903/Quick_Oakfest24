05651ea25545b2fa99c564a4520b9657b1db25cd
ionlymadethisaccountforjavascript <108260879+ionlymadethisaccountforjavascript@users.noreply.github.com>
2024-01-20 14:44:35 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,19 @@
 const username = document.getElementById('username')
 const password = document.getElementById('password')
+const email = document.getElementById('email')
 const form = document.getElementById('form')
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const userNameValue = username.value;
-    const passwordValue = password.value;               
+    const passwordValue = password.value;
+    const emailValue = email.value;               
     localStorage.setItem('username', userNameValue);
-    localStorage.setItem('password',passwordValue)   
-    window.location.href = 'mainpage.html';
+    localStorage.setItem('password',passwordValue);
+    localStorage.setItem('email',emailValue)   
+
+
+})
+
 
 })
 
