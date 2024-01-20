bd535c235b63da06536d1d6ad0a7098201838032
quick no. 2 <157027960+ilyxr@users.noreply.github.com>
2024-01-20 11:15:05 +0530

    Update Ashmit-News.py
    Import modules & initializing NTLK for NLP

--- Ashmit-News.py
+++ Ashmit-News.py
@@ -1,1 +1,13 @@
+import openpyxl
+import newsapi
+import yfinance as yf
+import pandas as pd
+import datetime as datetime, timedelta
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+# Set up NLTK
+nltk_stopwords = set(stopwords.words('english'))
+sid = SentimentIntensityAnalyzer()
