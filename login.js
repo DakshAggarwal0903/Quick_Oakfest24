12ae985b85b38a4e382a94dddf2c24c2e47f7056
ionlymadethisaccountforjavascript <108260879+ionlymadethisaccountforjavascript@users.noreply.github.com>
2024-01-20 12:32:52 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,13 @@
 const username = document.getElementById('username')
+const password = document.getElementById('password')
 const form = document.getElementById('form')
 form.addEventListener('submit', function(e){
-    //add event listener to listen for certain commands
-    e.preventDefault();//prevents normal processing of form
-    const userNameValue = username.value;                
-    localStorage.setItem('username', userNameValue);   
+    e.preventDefault();
+    const userNameValue = username.value;
+    const passwordValue = password.value;               
+    localStorage.setItem('username', userNameValue);
+    localStorage.setItem('password',passwordValue)   
     window.location.href = 'mainpage.html';
+
 })
+
