043198f17efe39cd115c5591e8b92be87805cf51
Trashyyy396 <119867127+Trashyyy396@users.noreply.github.com>
2024-01-21 11:04:24 +0530

    Create password.js

---
+++ password.js
@@ -0,0 +1,14 @@
+function redirectToMainPage() {
+    // Dummy check for username and password (you should implement proper authentication)
+    var username_prompt = document.getElementById('username').value;
+    var password_prompt = document.getElementById('password').value;
+    const username = 'user'
+    const password = 'pass'
+    // Example: Redirect to the main page if username and password are not empty
+    if (username_prompt == username && password_prompt == password) {
+      window.location.href = 'fined.html';
+    } else {
+      alert('Please enter valid credentials.');
+    }
+  }
+
